a954d7bf8e00762544e04cf1d6d079251446e854
Luis Gustavo <luis.gustavo.sene99@gmail.com>
2024-07-01 02:39:45 -0300

    Update
    Hoje eu comentei o que cada bloco de codigo representa, isso e bom para que os leitores se identifiquem com o codigo mais rápido.

--- src/main/java/org/adivinhacao/DAO/UserDAO.java
+++ src/main/java/org/adivinhacao/DAO/UserDAO.java
@@ -12,25 +12,25 @@ import java.util.Scanner;
 public class UserDAO {
 
     public void registeringUser(user ranked) {
-
         Scanner sc = new Scanner(System.in);
 
+        //This block of code create a random number.
         Random number = new Random();
         int secret = number.nextInt(100);
 
+        System.out.println(secret);
+
         System.out.println("Create your nick name:");
-        String nicks = sc.nextLine();
-
-        System.out.println("\nChoose the mode | Easy - [1] | Normal - [2] | Hard - [3]\n");
-
-        String nivel = sc.nextLine();
-        int levels = 5;
-        levels = Integer.parseInt(nivel);
+        String nick = sc.nextLine();
 
         //This variable count how chances the user have.
         int fix = 15;
 
-        System.out.println("Choose a number from the 0 of 100\n");
+        //This block of code selects between three game mode: easy, normal and hard.
+        System.out.println("\nChoose the mode | Easy - [1] | Normal - [2] | Hard - [3]\n");
+        String nivel = sc.nextLine();
+        int levels = 5;
+        levels = Integer.parseInt(nivel);
 
         switch (levels) {
             case 1:
@@ -53,48 +53,29 @@ public class UserDAO {
                 break;
         }
 
+        System.out.println("Choose a number from the 0 of 100\n");
+
+        //This block of code read of the user input and the decision of structure read and executa
         int i = 1;
         while (i <= levels) {
             String attempt = sc.nextLine();
             int kick = Integer.parseInt(attempt);
 
             boolean right = kick == secret;
-            boolean smaller = kick < secret;
-            boolean bigger = kick > secret;
 
             if (kick <= 0) {
                 System.out.println("You cannot input negative number \nTry again");
                 i--;
                 continue;
-            }
-
-            if (right) {
-                System.out.println("╚═( ͡° ͜ʖ ͡°)═╝\n" +
-                        "╚═(███)═╝\n" +
-                        "╚═(███)═╝\n" +
-                        ".╚═(███)═╝\n" +
-                        "..╚═(███)═╝\n" +
-                        "…╚═(███)═╝\n" +
-                        "…╚═(███)═╝\n" +
-                        "..╚═(███)═╝\n" +
-                        ".╚═(███)═╝\n" +
-                        "╚═(███)═╝\n" +
-                        ".╚═(███)═╝\n" +
-                        "..╚═(███)═╝\n" +
-                        "…╚═(███)═╝\n" +
-                        "…╚═(███)═╝\n" +
-                        "…..╚(███)╝\n" +
-                        "……╚(██)╝\n" +
-                        "………(█)");
+            } if (right) {
                 break;
-            }
-            if (smaller) {
+            } else if (kick < secret) {
                 System.out.println("\nGuess is smaller than the secret number");
                 System.out.println("You have a " + fix + " chance");
                 System.out.println("\nTry again");
                 i++;
                 fix--;
-            } if (bigger) {
+            } else {
                 System.out.println("\nGuess is greater than the secret number");
                 System.out.println("You have a " + fix + " chance");
                 System.out.println("\nTry again");
@@ -103,15 +84,42 @@ public class UserDAO {
             }
         }
 
+        //This block of code prints a win or loss message.
+        String attempt = sc.nextLine();
+        int kick = Integer.parseInt(attempt);
+
+        boolean right = kick == secret;
+
+        if (right) {
+            System.out.println("╚═( ͡° ͜ʖ ͡°)═╝\\n\" +\n" +
+                    "                        \"╚═(███)═╝\\n\" +\n" +
+                    "                        \"╚═(███)═╝\\n\" +\n" +
+                    "                        \".╚═(███)═╝\\n\" +\n" +
+                    "                        \"..╚═(███)═╝\\n\" +\n" +
+                    "                        \"…╚═(███)═╝\\n\" +\n" +
+                    "                        \"…╚═(███)═╝\\n\" +\n" +
+                    "                        \"..╚═(███)═╝\\n\" +\n" +
+                    "                        \".╚═(███)═╝\\n\" +\n" +
+                    "                        \"╚═(███)═╝\\n\" +\n" +
+                    "                        \".╚═(███)═╝\\n\" +\n" +
+                    "                        \"..╚═(███)═╝\\n\" +\n" +
+                    "                        \"…╚═(███)═╝\\n\" +\n" +
+                    "                        \"…╚═(███)═╝\\n\" +\n" +
+                    "                        \"…..╚(███)╝\\n\" +\n" +
+                    "                        \"……╚(██)╝\\n\" +\n" +
+                    "                        \"………(█)");
+        } else {
+            System.out.println("\\\\n─█───█▀▀█─█▀▀─█▀▀─█▀▀─█▀▀█\\\\n\\\" +\\n\" +\n" +
+                    "                        \"                \\\"─█───█──█─▀▀█─▀▀█─█▀▀─█▄▄▀\\\\n\\\" +\\n\" +\n" +
+                    "                        \"                \\\"─▀▀▀─▀▀▀▀─▀▀▀─▀▀▀─▀▀▀─▀─▀▀");
+        }
+
+        //This block of code adds the user's score.
         double points = 1000;
         double score = points * 2 / 3;
         System.out.println("Your score is " + score + "\n\n");
 
-
-        System.out.println("\n─█───█▀▀█─█▀▀─█▀▀─█▀▀─█▀▀█\n" +
-                "─█───█──█─▀▀█─▀▀█─█▀▀─█▄▄▀\n" +
-                "─▀▀▀─▀▀▀▀─▀▀▀─▀▀▀─▀▀▀─▀─▀▀");
-
+        //This block of code insert the data in the SQL database.
         String sql = "INSERT INTO RANKED (NICK, SCORE) VALUES (?, ?)";
 
         PreparedStatement ps = null;
@@ -119,7 +127,7 @@ public class UserDAO {
         try {
             ps = conection.getConection().prepareStatement(sql);
 
-            ps.setString(1, user.getNick(nicks));
+            ps.setString(1, user.getNick(nick));
             ps.setDouble(2, user.getScore(score));
 
             ps.execute();
